bb3d337d405aed5e35e87db727199c451f0ad8b3
tjxt <929121410@qq.com>
2026-05-14 22:09:31 +0800

    完整实现了人脸打卡的上下班打卡以及计算工作时间和记录考勤状态

--- manage/src/main/java/com/mwh/service/impl/AttendanceServiceImpl.java
+++ manage/src/main/java/com/mwh/service/impl/AttendanceServiceImpl.java
@@ -1,5 +1,4 @@
 package com.mwh.service.impl;
-
 import cn.hutool.json.JSONUtil;
 import com.baomidou.mybatisplus.core.conditions.query.LambdaQueryWrapper;
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
@@ -15,12 +14,12 @@ import com.mwh.vo.IsAttendOk;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
-
 import java.math.BigDecimal;
+import java.math.RoundingMode;
+import java.time.Duration;
 import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.time.LocalTime;
-
 /**
 * @author mawenhan
 * @description 针对表【attendance(考勤记录表)】的数据库操作Service实现
@@ -35,6 +34,7 @@ public class AttendanceServiceImpl extends ServiceImpl<AttendanceMapper, Attenda
     private final AttendanceRuleMapper attendanceRuleMapper;
     /**
      * 创建打卡记录
+     * TODO 第一次打开后可以将数据存redis中
      * @return
      */
     @Override
@@ -66,7 +66,6 @@ public class AttendanceServiceImpl extends ServiceImpl<AttendanceMapper, Attenda
                 attendance.setAttendanceDate(LocalDate.now());
                 attendance.setCheckInTime(faceDetectResult.getTime());
                 attendance.setStatus(attendanceResult.getStatus());
-                //TODO 考勤规则要实现如果迟到或者早退，要计算迟到或者早退的分钟数
                 attendance.setLateMinutes(attendanceResult.getLateTime());
                 attendance.setEarlyMinutes(0);
                 attendance.setWorkHours(BigDecimal.valueOf(0));
@@ -75,6 +74,19 @@ public class AttendanceServiceImpl extends ServiceImpl<AttendanceMapper, Attenda
                 save(attendance);
                 return new IsAttendOk(true, "打卡成功");
             }
+            // 打卡记录已存在这次打卡代表下班
+            AttendanceResult attendanceResult = checkOut(faceDetectResult);
+            if(one.getStatus() == AttendanceEnum.ATTENDANCE){
+                one.setStatus(attendanceResult.getStatus());
+            }
+            one.setCheckOutTime(LocalDateTime.now());
+            one.setEarlyMinutes(attendanceResult.getLateTime());
+            long minutes = Duration.between(one.getCheckInTime(), one.getCheckOutTime()).toMinutes();
+            BigDecimal workHours = BigDecimal.valueOf(minutes)
+                    .divide(BigDecimal.valueOf(60), 2, RoundingMode.HALF_UP);
+            one.setWorkHours(workHours);
+            updateById(one);
+            return new IsAttendOk(true, "打卡成功");
         }
         return new IsAttendOk(false, "请勿重复打卡");
     }
@@ -93,18 +105,35 @@ public class AttendanceServiceImpl extends ServiceImpl<AttendanceMapper, Attenda
         LocalTime actualTime = actualDateTime.toLocalTime();
         if(actualTime.isAfter(parse) ){
             attendanceResult.setStatus(AttendanceEnum.LATE);
-            attendanceResult.setLateTime(faceDetectResult.getTime().getMinute() - parse.getMinute());
+            //迟到时间
+            attendanceResult.setLateTime((int)Duration.between(parse, actualTime).toMinutes());
             return attendanceResult;
         }
-        else {
-            attendanceResult.setStatus(AttendanceEnum.ATTENDANCE);
-            attendanceResult.setLateTime(0);
-            return attendanceResult;
-        }
+        attendanceResult.setStatus(AttendanceEnum.ATTENDANCE);
+        attendanceResult.setLateTime(0);
+        return attendanceResult;
     }
     /**
-     * 计算迟到时间
+     * 第二次打卡代表下班
      */
+    public AttendanceResult checkOut(FaceDetectResult faceDetectResult) {
+        String rulekey = "work_end_time";
+        LambdaQueryWrapper<AttendanceRule> attendanceRuleLambdaQueryWrapper = new LambdaQueryWrapper<>();
+        attendanceRuleLambdaQueryWrapper.eq(AttendanceRule::getRuleKey, rulekey);
+        AttendanceRule one = attendanceRuleMapper.selectOne(attendanceRuleLambdaQueryWrapper);
+        LocalTime parse = LocalTime.parse(one.getRuleValue());
+        AttendanceResult attendanceResult = new AttendanceResult();
+        LocalDateTime actualDateTime = faceDetectResult.getTime();  // 假设这是 LocalDateTime
+        LocalTime actualTime = actualDateTime.toLocalTime();
+        if(actualTime.isBefore(parse)){
+            attendanceResult.setStatus(AttendanceEnum.LATE);
+            attendanceResult.setLateTime((int)Duration.between(parse, actualTime).toMinutes());
+            return attendanceResult;
+        }
+        attendanceResult.setStatus(AttendanceEnum.ATTENDANCE);
+        attendanceResult.setLateTime(0);
+        return attendanceResult;
+    }
 
 }
 
